e6f78327a00768d4ef4f9e467b7d829d19e24b9d
Arizidane <samionohari@gmail.com>
2023-12-02 01:30:01 +0700

    fix: preloader, btn nav, feat: mode strg

--- script.js
+++ script.js
@@ -84,32 +84,37 @@ function msgBox(title, msg) {
 }
 
 document.querySelector('#resume').addEventListener('click', () => {
-	msgBox('Sorry', 'My Resume lost 2 week ago:(')
+	msgBox('Sorry', 'My Resume lost 2 week ago :(')
 })
 document.querySelector('#linked-in').addEventListener('click', () => {
-	msgBox('Sorry', 'I forgot my LinkedIn account password:(')
+	msgBox('Sorry', 'I forgot my LinkedIn account password :(')
 })
-document.querySelector('.no-repo').addEventListener('click', () => {
-	msgBox('Sorry', 'This section does not contain any repositories :(')
-})
+document.querySelectorAll('.no-repo').forEach(element => {
+	element.addEventListener('click', () => {
+		msgBox('Sorry', 'This section does not contain any repositories <br>:(')
+	})
+});
 
 
 // ________[Button mode Handler]_______
-let isParallaxMode = false
+if (!localStorage.getItem('isParallaxMode')) {
+	localStorage.setItem('isParallaxMode', false) // init
+} else if (localStorage.getItem('isParallaxMode' === true)) {
+	localStorage.setItem('isParallaxMode', false) // init value
+}
 
 document.querySelector('.mode').addEventListener('click', () => {
 	const bugElement = document.querySelector('.bug');
 	const virusElement = document.querySelector('.virus');
 
 	if (bugElement.style.display === 'none') {
-		noScrollMode()
 		bugElement.style.display = 'block';
 		virusElement.style.display = 'none';
-		isParallaxMode = false
-	} else {
+		localStorage.setItem('isParallaxMode', false)
+} else {
 		bugElement.style.display = 'none';
 		virusElement.style.display = 'block';
-		isParallaxMode = true
+		localStorage.setItem('isParallaxMode', true)
 	}
 	// console.log(isParallaxMode);
 });
@@ -135,60 +140,71 @@ function setActiveNavLink(event) {
 	}
 	event.currentTarget.classList.add('active');
 }
+// console.log(localStorage.getItem('isParallaxMode'));
 
-noScrollMode()
-function noScrollMode() {
-	if (isParallaxMode === true) {
-		return
-	} else {
+// noScrollMode()
+// document.querySelector('a[href="#about"').addEventListener('click', noScrollMode('a[href="#about"'))
+
+// console.log(document.querySelector('a[href="#about"'));
+
+function noScrollMode(event) {
+	if (localStorage.getItem('isParallaxMode') == 'false') {
+
 		const secList = ['.about', '#projects', '#Internship', '#skills', '.footer']
+		const activeSec = document.querySelector('.activeSection')
+		// console.log(event);
 
-		document.querySelector('a[href="#about"]').addEventListener('click', (event) => {
-			const aboutSec = document.querySelector('.about');
-
-			setActiveNavLink(event)
-			swipeTo('.activeSection', `translate(0, 0)`, `translate(0, 1200px)`)
-			document.querySelector('.activeSection').classList.remove('activeSection')
-			setTimeout(() => {
-				let sec = secList.slice(0, 0).concat(secList.slice(1))
-				sec.forEach((selector) => {
-					const element = document.querySelector(`${selector}`);
-					if (element) {
-						element.style.display = 'none';
-					}
-				});
-				aboutSec.style.display = 'flex'
+		if (event.srcElement.id == 'nav-about') {
+			if (activeSec) {
+				const aboutSec = document.querySelector('.about');
+	
+				setActiveNavLink(event)
+				swipeTo('.activeSection', `translate(0, 0)`, `translate(0, 1200px)`)
+				activeSec.classList.remove('activeSection')
 				aboutSec.classList.add('activeSection')
-				swipeTo('.about', `translate(0, -1200px)`, `translate(0, 0)`)
-			}, 700);
-		});
+				setTimeout(() => {
+					let sec = secList.slice(0, 0).concat(secList.slice(1))
+					sec.forEach((selector) => {
+						const element = document.querySelector(`${selector}`);
+						if (element) {
+							element.style.display = 'none';
+						}
+					});
+					aboutSec.style.display = 'flex'
+					swipeTo('.about', `translate(0, -1200px)`, `translate(0, 0)`)
+				}, 700);
+			}
+		}
 
-		document.querySelector('a[href="#projects"]').addEventListener('click', (event) => {
-			const projectsSec = document.querySelector('#projects');
-
-			setActiveNavLink(event)
-			swipeTo('.activeSection', `translate(0, 0)`, `translate(1300px, -1200px)`)
-			document.querySelector('.activeSection').classList.remove('activeSection')
-			setTimeout(() => {
-				let sec = secList.slice(0, 1).concat(secList.slice(2))
-				sec.forEach((selector) => {
-					const element = document.querySelector(`${selector}`);
-					if (element) {
-						element.style.display = 'none';
-					}
-				});
-				projectsSec.style.display = 'block'
+		if (event.srcElement.id == 'btn1') {
+			if (activeSec) {
+				const projectsSec = document.querySelector('#projects');
+		
+				setActiveNavLink(event)
+				swipeTo('.activeSection', `translate(0, 0)`, `translate(1300px, -1200px)`)
+				activeSec.classList.remove('activeSection')
 				projectsSec.classList.add('activeSection')
-				swipeTo('#projects', `translate(-1300px, 1200px)`, `translate(0, 0)`)
-			}, 700);
-		});
+				setTimeout(() => {
+					let sec = secList.slice(0, 1).concat(secList.slice(2))
+					sec.forEach((selector) => {
+						const element = document.querySelector(`${selector}`);
+						if (element) {
+							element.style.display = 'none';
+						}
+					});
+					projectsSec.style.display = 'block'
+					swipeTo('#projects', `translate(-1300px, 1200px)`, `translate(0, 0)`)
+				}, 700);
+			}
+		}
 
-		document.querySelector('a[href="#Internship"]').addEventListener('click', (event) => {
+		if (event.srcElement.id == 'btn2') {
 			const internshipSec = document.querySelector('#Internship');
-
+	
 			setActiveNavLink(event)
 			swipeTo('.activeSection', `translate(0, 0)`, `translate(-1300px, -1200px)`)
-			document.querySelector('.activeSection').classList.remove('activeSection')
+			activeSec.classList.remove('activeSection')
+			internshipSec.classList.add('activeSection')
 			setTimeout(() => {
 				let sec = secList.slice(0, 2).concat(secList.slice(3))
 				sec.forEach((selector) => {
@@ -198,17 +214,17 @@ function noScrollMode() {
 					}
 				});
 				internshipSec.style.display = 'block'
-				internshipSec.classList.add('activeSection')
 				swipeTo('#Internship', `translate(1300px, 1200px)`, `translate(0, 0)`)
 			}, 700);
-		});
+		}
 
-		document.querySelector('a[href="#skills"]').addEventListener('click', (event) => {
+		if (event.srcElement.id == 'btn3') {
 			const skillsSec = document.querySelector('#skills');
-
+	
 			setActiveNavLink(event)
 			swipeTo('.activeSection', `translate(0, 0)`, `translate(0, -1200px)`)
-			document.querySelector('.activeSection').classList.remove('activeSection')
+			activeSec.classList.remove('activeSection')
+			skillsSec.classList.add('activeSection')
 			setTimeout(() => {
 				let sec = secList.slice(0, 3).concat(secList.slice(4))
 				sec.forEach((selector) => {
@@ -218,41 +234,43 @@ function noScrollMode() {
 					}
 				});
 				skillsSec.style.display = 'block'
-				skillsSec.classList.add('activeSection')
 				swipeTo('#skills', `translate(0, 1200px)`, `translate(0, 0)`)
 			}, 700);
-		});
+		}
+	} else {
+		msgBox('Info', `Maybe you're on Parallax mode.<br>Click on Virus icon to open No Scroll mode.`)
 	}
 }
+
 
 
 // ________[Parallax per-Section Handle]________
-parallaxMode()
-function parallaxMode() {
-	if (isParallaxMode) {
-		document.addEventListener('scroll', () => {
-			const scrollValue = window.scrollY;
-			// console.log(scrollValue);
+// parallaxMode()
+// function parallaxMode() {
+// 	if (localStorage.getItem('isParallaxMode') == 'true') {
+// 		document.addEventListener('scroll', () => {
+// 			const scrollValue = window.scrollY;
+// 			// console.log(scrollValue);
 
-			if (scrollValue > 0) {
-				const divider = scrollValue
+// 			if (scrollValue > 0) {
+// 				const divider = scrollValue
 
-				const aboutSec = document.querySelector('.about');
+// 				const aboutSec = document.querySelector('.about');
 
-				aboutSec.style.transition = `all .3s ease`;
-				if (1 - divider * 0.002 > 0) {
-					aboutSec.style.transform = `translate3d(-${divider * 0.9}px, ${divider * 0.5}px, 0) scale(${1 - divider * 0.002})`
-				}
-				if (divider > 900) {
-					aboutSec.style.opacity = '0'
-				} else if (divider < 900) {
-					aboutSec.style.opacity = '1'
-				}
-				// console.log(divider);
-			}
-		})
-	}
-}
+// 				aboutSec.style.transition = `all .3s ease`;
+// 				if (1 - divider * 0.002 > 0) {
+// 					aboutSec.style.transform = `translate3d(-${divider * 0.9}px, ${divider * 0.5}px, 0) scale(${1 - divider * 0.002})`
+// 				}
+// 				if (divider > 900) {
+// 					aboutSec.style.opacity = '0'
+// 				} else if (divider < 900) {
+// 					aboutSec.style.opacity = '1'
+// 				}
+// 				// console.log(divider);
+// 			}
+// 		})
+// 	}
+// }
 
 
 // ________[Background handler]________
